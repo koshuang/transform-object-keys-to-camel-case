a8e400785b9d0a2877d4cf14e7a659262c066c28
Kos <koshuang@gmail.com>
2021-07-11 23:44:28 +0800

    refactor(function): [convert] remove nested block

--- convert.js
+++ convert.js
@@ -12,43 +12,43 @@ export function convert(user) {
 
   if (Object.keys(user) == []) {
     return user;
-  } else {
-    for (var key in user) {
-      const idx = key.indexOf('_') + 1;
-      let tempKey;
-      // key值改变
-      if (idx != 0) {
-        const up = key.substr(idx, 1); // 需要转位大写的地方
-        if (key.length - 1 == idx) {
-          tempKey = `${key.substr(0, idx - 1)}` + `${up.toUpperCase()}`;
-        } else {
-          tempKey =
-            `${key.substr(0, idx - 1)}` +
-            `${up.toUpperCase()}` +
-            `${key.substr(idx + 1)}`;
-        }
-      } else {
-        tempKey = key; //没有下划线
-      }
-
-      // 判断属性是否是对象
-      if (user[key] instanceof Object && !(user[key] instanceof Array)) {
-        returnUser[tempKey] = convert(user[key]);
-      } else if (user[key] instanceof Array) {
-        if (user[key].length == 0 || !(user[key][0] instanceof Object)) {
-          returnUser[tempKey] = user[key];
-        } else {
-          let tempArr = [];
-          for (v of user[key]) {
-            tempArr.push(convert(v));
-          }
-          returnUser[tempKey] = tempArr;
-        }
-      } else {
-        returnUser[tempKey] = user[key];
-        //对 A___多个下划线的情况没做处理 懒得写判断了
-      }
-    }
-    return returnUser;
   }
+
+  for (var key in user) {
+    const idx = key.indexOf('_') + 1;
+    let tempKey;
+    // key值改变
+    if (idx != 0) {
+      const up = key.substr(idx, 1); // 需要转位大写的地方
+      if (key.length - 1 == idx) {
+        tempKey = `${key.substr(0, idx - 1)}` + `${up.toUpperCase()}`;
+      } else {
+        tempKey =
+          `${key.substr(0, idx - 1)}` +
+          `${up.toUpperCase()}` +
+          `${key.substr(idx + 1)}`;
+      }
+    } else {
+      tempKey = key; //没有下划线
+    }
+
+    // 判断属性是否是对象
+    if (user[key] instanceof Object && !(user[key] instanceof Array)) {
+      returnUser[tempKey] = convert(user[key]);
+    } else if (user[key] instanceof Array) {
+      if (user[key].length == 0 || !(user[key][0] instanceof Object)) {
+        returnUser[tempKey] = user[key];
+      } else {
+        let tempArr = [];
+        for (v of user[key]) {
+          tempArr.push(convert(v));
+        }
+        returnUser[tempKey] = tempArr;
+      }
+    } else {
+      returnUser[tempKey] = user[key];
+      //对 A___多个下划线的情况没做处理 懒得写判断了
+    }
+  }
+  return returnUser;
 }
